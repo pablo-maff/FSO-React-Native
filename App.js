ad3b99bfe7f0b399ea24531463d8a855316c4828
pablo-maff <pablo.maff.076@gmail.com>
2022-05-16 14:06:46 +0200

    Fixed react native debugger error

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  console.log('logs!')
   return (
     <View style={styles.container}>
       <Text>Hello World!!</Text>
